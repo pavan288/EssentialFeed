c14cdf4f934b6fdfeeb1b77dfab1a246e1846375
Pavan Powani <pavan.powani@ectosense.com>
2021-06-05 15:30:07 +0530

    Use subclass of HTTPClient to run tests

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,19 +8,43 @@
 import XCTest
 
 class RemoteFeedLoader {
+    let client: HTTPClient
 
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    func load() {
+        client.get(from: URL(string: "https://a-url.com")!)
+    }
 }
 
-class HTTPCleint {
+class HTTPClient {
+    func get(from url: URL) {}
+}
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+
+    override func get(from url: URL) {
+        self.requestedURL = url
+    }
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPCleint()
-        let _ = RemoteFeedLoader()
+        let client = HTTPClientSpy()
+        let _ = RemoteFeedLoader(client: client)
 
         XCTAssertNil(client.requestedURL)
+    }
+
+    func test_init_requestDataFromURL() {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(client: client)
+
+        sut.load()
+
+        XCTAssertNotNil(client.requestedURL)
     }
 }
