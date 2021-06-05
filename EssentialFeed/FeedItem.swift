e24cf2031cbdb1879c4058318e253f181a04fcbf
Pavan Powani <pavan.powani@ectosense.com>
2021-06-05 14:24:02 +0530

    Add FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Pavan Powani on 05/06/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let location: String?
+    let description: String?
+    let imageURL: URL
+}
