1284975c2fbc8e1975ba0f84f38ae9e9f11719d3
Pavan Powani <pavan.powani@ectosense.com>
2021-06-19 19:38:38 +0530

    Delivers items array on 200 http response with json items

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -72,6 +72,41 @@ class RemoteFeedLoaderTests: XCTestCase {
         }
     }
 
+    func test_load_deliversItemsOn200HTTPResponseWithJSONItems() {
+        let (sut, client) = makeSUT()
+
+        let item1 = FeedItem(id: UUID(),
+                             location: nil,
+                             description: nil,
+                             imageURL: URL(string: "https://a-url.com")!)
+
+        let item1JSON = [
+            "id": item1.id.uuidString,
+            "image": item1.imageURL.absoluteString
+        ]
+
+        let item2 = FeedItem(id: UUID(),
+                             location: "a location",
+                             description: "a description",
+                             imageURL: URL(string: "https://another-url.com")!)
+
+        let item2JSON = [
+            "id": item2.id.uuidString,
+            "location": item2.location,
+            "description": item2.description,
+            "image": item2.imageURL.absoluteString
+        ]
+
+        let itemsJSON = [
+            "items": [item1JSON, item2JSON]
+        ]
+
+        expect(sut, toCompleteWithResult: .success([item1, item2])) {
+            let jsonData = try! JSONSerialization.data(withJSONObject: itemsJSON)
+            client.complete(withStatusCode: 200, data: jsonData)
+        }
+    }
+
     private func expect(_ sut: RemoteFeedLoader, toCompleteWithResult result: RemoteFeedLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
         var capturedResults = [RemoteFeedLoader.Result]()
         sut.load { capturedResults.append($0) }
