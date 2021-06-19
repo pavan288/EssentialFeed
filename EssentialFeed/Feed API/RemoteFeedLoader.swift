ede3b1fa26ffe639eb0a8d6fa26c1cf92d5d7b36
Pavan Powani <pavan.powani@ectosense.com>
2021-06-19 21:28:02 +0530

    Move mapping logic to new type FeedItemsMapper

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -39,9 +39,8 @@ public final class RemoteFeedLoader {
         client.get(from: url) { (result) in
             switch result {
             case let .success(data, response):
-                if let root = try?  JSONDecoder().decode(Root.self, from: data),
-                   response.statusCode == 200 {
-                    completion(.success(root.items.map({ $0.item })))
+                if let items = try? FeedItemsMapper.map(data, response) {
+                    completion(.success(items))
                 } else {
                     completion(.failure(.invalidData))
                 }
@@ -52,17 +51,26 @@ public final class RemoteFeedLoader {
     }
 }
 
-private struct Root: Decodable {
-    let items: [Item]
-}
+private class FeedItemsMapper {
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == 200 else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+        return try JSONDecoder().decode(Root.self, from: data).items.map({ $0.item })
+    }
 
-private struct Item: Decodable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let image: URL
+    private struct Root: Decodable {
+        let items: [Item]
+    }
 
-    var item: FeedItem {
-        return FeedItem(id: id, location: location, description: description, imageURL: image)
+    private struct Item: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+
+        var item: FeedItem {
+            return FeedItem(id: id, location: location, description: description, imageURL: image)
+        }
     }
 }
