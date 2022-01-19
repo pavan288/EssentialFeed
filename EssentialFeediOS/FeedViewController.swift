23ad75309e390be16723683c0f10f74098d8c292
Pavan Powani <pavan.powani@ectosense.com>
2022-01-19 19:10:43 +0530

    Feed image view retry button is visible on invalid loaded image data

--- EssentialFeediOS/FeedViewController.swift
+++ EssentialFeediOS/FeedViewController.swift
@@ -63,8 +63,9 @@ public final class FeedViewController: UITableViewController {
         cell.feedImageContainer.startShimmering()
         tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url) { [weak cell] result in
             let imageData = try? result.get()
-            cell?.feedImageView.image = imageData.map(UIImage.init) ?? nil
-            cell?.feedImageRetryButton.isHidden = (imageData != nil)
+            let image = imageData.map(UIImage.init) ?? nil
+            cell?.feedImageView.image = image
+            cell?.feedImageRetryButton.isHidden = (image != nil)
             cell?.feedImageContainer.stopShimmering()
         }
         return cell
