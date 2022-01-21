19fc59a23bda16a53345565626b62d9d8dbd3a74
Pavan Powani <pavan.powani@ectosense.com>
2022-01-21 15:08:29 +0530

    Extract FeedImageCell test helpers to a separate file

--- EssentialFeediOSTests/Controllers/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/Controllers/FeedViewControllerTests.swift
@@ -331,33 +331,3 @@ class LoaderSpy: FeedLoader, FeedImageDataLoader {
         imageRequests[index].completion(.failure(error))
     }
 }
-
-private extension FeedImageCell {
-    var isShowingLocation: Bool {
-        return !locationContainer.isHidden
-    }
-
-    var locationText: String? {
-        return locationLabel.text
-    }
-
-    var descriptionText: String? {
-        return descriptionLabel.text
-    }
-
-    var isShowingImageLoadingIndicator: Bool {
-        return feedImageContainer.isShimmering
-    }
-
-    var renderedImage: Data? {
-        return feedImageView.image?.pngData()
-    }
-
-    var isShowingRetryAction: Bool {
-        return !feedImageRetryButton.isHidden
-    }
-
-    func simulateRetryAction() {
-        feedImageRetryButton.simulateTap()
-    }
-}
