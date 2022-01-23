8fe1575c4abd0e8f412fa9e7fb8344de0bc667c6
Pavan Powani <pavan.powani@ectosense.com>
2022-01-23 17:22:30 +0530

    Move ViewModels to separate files

---
+++ FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Pavan Powani on 23/01/22.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
