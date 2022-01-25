edd9e9fbbd3b2f6c3bbddb50f9e6d96cf2fed4d8
Pavan Powani <pavan.powani@ectosense.com>
2022-01-25 15:56:07 +0530

    Move FeedLoaderPresentationAdapter to separate file

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -56,25 +56,3 @@ private final class FeedViewAdapter: FeedView {
         }
     }
 }
-
-private final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
-    private let feedLoader: FeedLoader
-    var feedPresenter: FeedPresenter?
-
-    init(feedLoader: FeedLoader) {
-        self.feedLoader = feedLoader
-    }
-
-    func didRequestFeedRefresh() {
-        feedPresenter?.didStartLoadingFeed()
-
-        feedLoader.load { [weak self] result in
-            switch result {
-            case .success(let feed):
-                self?.feedPresenter?.didFinishLoadingFeed(with: feed)
-            case .failure(let error):
-                self?.feedPresenter?.didFinishLoadingFeed(with: error)
-            }
-        }
-    }
-}
