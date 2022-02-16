ab682944a6fdb2fa9788010a9961e3c9147df520
Pavan Powani <pavan.powani@ectosense.com>
2022-02-16 15:37:26 +0530

    LocalFeedImageDataLoader.saveImageDataForURL requests image data insertion for url into the store

--- EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
@@ -10,35 +10,41 @@ import Foundation
 public final class LocalFeedImageDataLoader: FeedImageDataLoader {
     private final class Task: FeedImageDataLoaderTask {
         private var completion: ((FeedImageDataLoader.Result) -> Void)?
-
+        
         init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
             self.completion = completion
         }
-
+        
         func complete(with result: FeedImageDataLoader.Result) {
             completion?(result)
         }
-
+        
         func cancel() {
             preventFurtherCompletions()
         }
-
+        
         private func preventFurtherCompletions() {
             completion = nil
         }
     }
-
+    
     public enum Error: Swift.Error {
         case failed
         case notFound
     }
-
+    
     private let store: FeedImageDataStore
-
+    
     public init(store: FeedImageDataStore) {
         self.store = store
     }
-
+    
+    public typealias SaveResult = Result<Void, Swift.Error>
+    
+    public func save(_ data: Data, for url: URL, completion: @escaping (SaveResult) -> Void) {
+        store.insert(data, for: url) { _ in }
+    }
+    
     public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
         let task = Task(completion)
         store.retrieve(dataForURL: url) { [weak self] result in
