ec16755cd5ffddf7379fc7a37e2358ffd8684b4c
Pavan Powani <pavan.powani@ectosense.com>
2022-02-16 15:55:52 +0530

    LocalFeedImageDataLoader.saveImageDataFromURL succeeds on successful store insertion

--- EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public final class LocalFeedImageDataLoader {
     private let store: FeedImageDataStore
-    
+
     public init(store: FeedImageDataStore) {
         self.store = store
     }
@@ -17,46 +17,46 @@ public final class LocalFeedImageDataLoader {
 
 extension LocalFeedImageDataLoader {
     public typealias SaveResult = Result<Void, Swift.Error>
-    
+
     public enum SaveError: Error {
         case failed
     }
-    
+
     public func save(_ data: Data, for url: URL, completion: @escaping (SaveResult) -> Void) {
         store.insert(data, for: url) { result in
-            completion(.failure(SaveError.failed))
+            completion(result.mapError { _ in SaveError.failed })
         }
     }
 }
 
 extension LocalFeedImageDataLoader: FeedImageDataLoader {
     public typealias LoadResult = FeedImageDataLoader.Result
-    
+
     public enum LoadError: Swift.Error {
         case failed
         case notFound
     }
-    
+
     private final class LoadImageDataTask: FeedImageDataLoaderTask {
         private var completion: ((FeedImageDataLoader.Result) -> Void)?
-        
+
         init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
             self.completion = completion
         }
-        
+
         func complete(with result: FeedImageDataLoader.Result) {
             completion?(result)
         }
-        
+
         func cancel() {
             preventFurtherCompletions()
         }
-        
+
         private func preventFurtherCompletions() {
             completion = nil
         }
     }
-    
+
     public func loadImageData(from url: URL, completion: @escaping (LoadResult) -> Void) -> FeedImageDataLoaderTask {
         let task = LoadImageDataTask(completion)
         store.retrieve(dataForURL: url) { [weak self] result in
