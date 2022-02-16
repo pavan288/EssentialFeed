b193148c31ac883cbb000be8f6bb1d8c782eb07f
Pavan Powani <pavan.powani@ectosense.com>
2022-02-16 16:29:43 +0530

    CoreDataFeedStore.retrieveImageData delivers last inserted value (overwriting previous values)

--- EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
@@ -11,32 +11,44 @@ import XCTest
 class CoreDataFeedImageDataStoreTests: XCTestCase {
     func test_retrieveImageData_deliversNotFoundWhenEmpty() {
         let sut = makeSUT()
-
+        
         expect(sut, toCompleteRetrievalWith: notFound(), for: anyURL())
     }
-
+    
     func test_retrieveImageData_deliversNotFoundWhenStoredDataURLDoesNotMatch() {
         let sut = makeSUT()
         let url = URL(string: "http://a-url.com")!
         let nonMatchingURL = URL(string: "http://another-url.com")!
-
+        
         insert(anyData(), for: url, into: sut)
-
+        
         expect(sut, toCompleteRetrievalWith: notFound(), for: nonMatchingURL)
     }
-
+    
     func test_retrieveImageData_deliversFoundDataWhenThereIsAStoredImageDataMatchingURL() {
         let sut = makeSUT()
         let storedData = anyData()
         let matchingURL = URL(string: "http://a-url.com")!
-
+        
         insert(storedData, for: matchingURL, into: sut)
-
+        
         expect(sut, toCompleteRetrievalWith: found(storedData), for: matchingURL)
     }
-
+    
+    func test_retrieveImageData_deliversLastInsertedValue() {
+        let sut = makeSUT()
+        let firstStoredData = Data("first".utf8)
+        let lastStoredData = Data("last".utf8)
+        let url = URL(string: "http://a-url.com")!
+        
+        insert(firstStoredData, for: url, into: sut)
+        insert(lastStoredData, for: url, into: sut)
+        
+        expect(sut, toCompleteRetrievalWith: found(lastStoredData), for: url)
+    }
+    
     // - MARK: Helpers
-
+    
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> CoreDataFeedStore {
         let storeBundle = Bundle(for: CoreDataFeedStore.self)
         let storeURL = URL(fileURLWithPath: "/dev/null")
@@ -44,26 +56,26 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
-
+    
     private func notFound() -> FeedImageDataStore.RetrievalResult {
         return .success(.none)
     }
-
+    
     private func found(_ data: Data) -> FeedImageDataStore.RetrievalResult {
         return .success(data)
     }
-
+    
     private func localImage(url: URL) -> LocalFeedImage {
         return LocalFeedImage(id: UUID(), location: "any", description: "any", url: url)
     }
-
+    
     private func expect(_ sut: CoreDataFeedStore, toCompleteRetrievalWith expectedResult: FeedImageDataStore.RetrievalResult, for url: URL,  file: StaticString = #file, line: UInt = #line) {
         let exp = expectation(description: "Wait for load completion")
         sut.retrieve(dataForURL: url) { receivedResult in
             switch (receivedResult, expectedResult) {
             case let (.success( receivedData), .success(expectedData)):
                 XCTAssertEqual(receivedData, expectedData, file: file, line: line)
-
+                
             default:
                 XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
             }
@@ -71,7 +83,7 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
         }
         wait(for: [exp], timeout: 1.0)
     }
-
+    
     private func insert(_ data: Data, for url: URL, into sut: CoreDataFeedStore, file: StaticString = #file, line: UInt = #line) {
         let exp = expectation(description: "Wait for cache insertion")
         let image = localImage(url: url)
@@ -79,7 +91,7 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
             switch result {
             case let .failure(error):
                 XCTFail("Failed to save \(image) with error \(error)", file: file, line: line)
-
+                
             case .success:
                 sut.insert(data, for: url) { result in
                     if case let Result.failure(error) = result {
