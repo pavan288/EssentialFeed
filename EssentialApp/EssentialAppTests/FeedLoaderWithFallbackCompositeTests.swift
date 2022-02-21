2e6b1b6abf3e3541cd1261acf7b621cb76660fa0
Pavan Powani <pavan.powani@ectosense.com>
2022-02-21 18:57:05 +0530

    Extract test helpers into a shared scope to remove duplication

--- EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -63,12 +63,8 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
 
-    private func anyNSError() -> NSError {
-        return NSError(domain: "any error", code: 0)
-    }
-
     private func uniqueFeed() -> [FeedImage] {
-        return [FeedImage(id: UUID(), location: "any", description: "any", url: URL(string: "http://any-url.com")!)]
+        return [FeedImage(id: UUID(), location: "any", description: "any", url: anyURL())]
     }
 
     private class LoaderStub: FeedLoader {
--- EssentialApp/EssentialAppTests/Helpers/XCTestCase+MemoryLeakTracking.swift
+++ EssentialApp/EssentialAppTests/Helpers/XCTestCase+MemoryLeakTracking.swift
@@ -5,7 +5,7 @@
 //  Created by Pavan Powani on 21/02/22.
 //
 
-import Foundation
+import XCTest
 
 extension XCTestCase {
     func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
